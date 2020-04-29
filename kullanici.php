e992f92dca20efd04b92092df8e204537d0f76f1
manolyailhan <36919728+manolyailhan@users.noreply.github.com>
2020-04-29 18:55:18 +0300

    Create kullanici.php

---
+++ kullanici.php
@@ -0,0 +1,179 @@
+<fieldset height="1000" >
+
+
+
+<legend color="red" border="200"><h3>Kullanıcı Sayfanız</h3></legend>
+<body>
+<body bgcolor="red">
+
+      <body style="background:url(akbank.jpg)  top center no-repeat;">
+	   
+
+
+<input type="button" name="button" id="buton" value="Çıkış Yap" onclick="window.location ='http://localhost/logout.php'"></input>
+
+	  
+
+         <form action="anketoyla.php" class="<div class="form-group" method="post">
+
+
+
+      <td colspan="2" align="left"><b>&nbsp;&nbsp;&nbsp;&nbsp;Hangi İlçede ki ATM'yi kullanıyorsunuz?</td>
+	  
+
+<div class="form-group">
+							<br><td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;<label for="states">İlçe Seçiniz</label> <br>
+							<br><td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;<select id="states" class="form-control" name="b_ilce">
+							
+								<option value="1">Aliağa</option>
+								<option value="2">Balçova</option>
+								<option value="3">Bayındır</option>
+								<option value="4">Bayraklı</option>
+								<option value="5">Bergama</option>
+								<option value="6">Beydağ</option>
+								<option value="7">Bornova</option>
+								<option value="8">Buca</option>
+								<option value="9">Çeşme</option>
+								<option value="10">Çiğli</option>
+								<option value="11">Dikili</option>
+								<option value="12">Foça</option>
+								<option value="13">Gaziemir</option>
+								<option value="14">Güzelbahçe</option>
+								<option value="15">Karabağlar</option>
+								<option value="16">Karaburun</option>
+								<option value="17">Karşıyaka</option>
+								<option value="18">Kemalpaşa</option>
+								<option value="19">Kınık</option>
+								<option value="20">Kiraz</option>
+								<option value="21">Konak</option>
+								<option value="22">Menderes</option>
+								<option value="23">Menemen</option>
+								<option value="24">Narlıdere</option>
+								<option value="25">Ödemiş</option>
+							</select>
+							<br>
+							
+							
+							
+							<br><td colspan="2" align="center">&nbsp;&nbsp;&nbsp;&nbsp;<label for="states">Bölge Seçiniz</label> <br>
+						    <br>&nbsp;&nbsp;&nbsp;&nbsp;<select id="states" class="form-control" name="b_ilce">
+								<option value="1">Aliağa-Aşağışakran Mahallesi</option>
+								<option value="1">Aliağa-Bahçedere Mahallesi</option>
+								<option value="3">Balçova-Fevzi Çakmak Mahallesi</option>
+								<option value="4">Balçova-Teleferik Mahallesi</option>
+								<option value="5">Bayındır-Alanköy Mahallesi</option>
+								<option value="6">Bayındır-Alankıyı Mahallesi</option>
+								<option value="7">Bayraklı-Mansuroğlu Mahallesi</option>
+								<option value="8">Bayraklı-Cengizhan Mahallesi</option>
+								<option value="9">Bergama-Ertuğrul Mahallesi</option>
+								<option value="10">Bergama-Fatih Mahallesi</option>
+								<option value="11">Beydağ- Adaküre Mahallesi</option>
+								<option value="12">Beydağ-Çamlık Mahallesi</option>
+								<option value="13">Bornova-Birlik Mahallesi</option>
+								<option value="14">Bornova-Çınar Mahallesi</option>
+								<option value="15">Buca-Adatepe Mahallesi</option>
+								<option value="16">Buca-Kuruçeşme Mahallesi</option>
+								<option value="17">Çeşme-Alaçatı Mahallesi</option>
+								<option value="18">Çeşme-Ardıç Mahallesi</option>
+								<option value="19">Çiğli-Aydınlıkevler Mahallesi</option>
+								<option value="20">Çiğli-Balatçık Mahallesi</option>
+								<option value="21">Dikili-Bademli Mahallesi</option>
+								<option value="22">Dikili-Çandarlı Mahallesi</option>
+								<option value="23">Foça-Hacıveli Mahallesi</option>
+								<option value="24">Foça-Ilıpınar Mahallesi</option>
+								<option value="25">Gaziemir-Beyazevler Mahallesi</option>
+								<option value="6">Gaziemir-Emrez Mahallesi</option>
+								<option value="7">Güzelbahçe-Payamlı Mahallesi</option>
+								<option value="8">Güzelbahçe-Maltepe Mahallesi</option>
+								<option value="9">Karabağlar-Aşık Veysel Mahallesi</option>
+								<option value="10">Karabağlar-Bahar Mahallesi</option>
+								<option value="11">Karaburun-İskele Mahallesi</option>
+								<option value="12">Karaburun-Kösedere Mahallesi</option>
+								<option value="13">Karşıyaka-Alaybey Mahallesi</option>
+								<option value="14">Karşıyaka-Aksoy Mahallesi</option>
+								<option value="15">Kemalpaşa-29 Ekim Mahallesi</option>
+								<option value="16">Kemalpaşa-Ansızca Mahallesi</option>
+								<option value="17">Kınık-Arpaseki Mahallesi</option>
+								<option value="18">Kınık-Balaban Mahallesi</option>
+								<option value="19">Kiraz-Pınarbaşı Mahallesi</option>
+								<option value="20">Kiraz-Ahmetler Mahallesi</option>
+								<option value="21">Konak-Akdeniz Mahallesi</option>
+								<option value="22">Konak-Akıncı Mahallesi</option>
+								<option value="23">Menderes-Ata Mahallesi</option>
+								<option value="24">Menderes-Bulgurca Mahallesi</option>
+								<option value="25">Menemen-Buruncuk Mahallesi</option>
+								<option value="10">Menemen-İsmet İnönü Mahallesi</option>
+								<option value="11">Narlıdere-Çamtepe Mahallesi</option>
+								<option value="12">Narlıdere-Ilıca Mahallesi</option>
+								<option value="13">Ödemiş-Bademli Mahallesi</option>
+								<option value="14">Ödemiş-Bayırlı Mahallesi</option>
+								
+							</select>
+							
+    </tr><br>
+	<br>
+	
+	
+	<td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Hizmetimizden Memnun Musunuz?</td>
+	
+	<br>
+	
+	
+	<br><td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="soru" value="evet"/>Evet<br/>
+
+	<br><td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="soru" value="hayır"/>Hayır<br/>
+	<br>
+
+<br><td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="submit" value="Cevabımı ilet"></input>
+</form>
+
+      </tr>
+</table>
+
+
+
+<style type="text/css">
+ 
+
+
+	
+
+
+#cikis{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:70px;
+	font-family:Arial;
+	}
+	
+#buton{
+	
+
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:1100px;
+	font-family:Arial;
+	}
+}
+</style>
+</fieldset>
