b8133e22d04522e2db58605f18b4d20ba9b44a0e
manolyailhan <36919728+manolyailhan@users.noreply.github.com>
2020-04-29 18:52:39 +0300

    Create ayar.php

---
+++ ayar.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$username = "aliarslan@yonetici";
+$password = "987654321";
+
+$kullanici = "manolya@ilhan";
+$sifre = "123456789";
+
+$user = "busra@topcu";
+$pass = "123456789";
+
+
+
+?>
