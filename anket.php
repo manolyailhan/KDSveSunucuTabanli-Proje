906892b00a9d05cadfea82280e4ada840245b870
manolyailhan <36919728+manolyailhan@users.noreply.github.com>
2020-04-29 18:51:56 +0300

    Create anket.php

---
+++ anket.php
@@ -0,0 +1,52 @@
+<form action="anketoyla.php" method="post">
+<tr>
+
+
+      <td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Hangi İlçede ki ATM'yi kullanıyorsunuz?</td>
+
+<div class="form-group">
+							<label for="states">İlçe Seçiniz</label>
+							<select id="states" class="form-control" name="b_ilce">
+								<option value="1">Aliağa</option>
+								<option value="2">Balçova</option>
+								<option value="3">Bayındır</option>
+								<option value="4">Bayraklı</option>
+								<option value="5">Bergama</option>
+								<option value="6">Beydağ</option>
+								<option value="7">Bornova</option>
+								<option value="8">Buca</option>
+								<option value="9">Çeşme</option>
+								<option value="10">Çiğli</option>
+								<option value="11">Dikili</option>
+								<option value="12">Foça</option>
+								<option value="13">Gaziemir</option>
+								<option value="14">Güzelbahçe</option>
+								<option value="15">Karabağlar</option>
+								<option value="16">Karaburun</option>
+								<option value="17">Karşıyaka</option>
+								<option value="18">Kemalpaşa</option>
+								<option value="19">Kınık</option>
+								<option value="20">Kiraz</option>
+								<option value="21">Konak</option>
+								<option value="22">Menderes</option>
+								<option value="23">Menemen</option>
+								<option value="24">Narlıdere</option>
+								<option value="25">Ödemiş</option>
+							</select>
+							<label for="states">Bölge Seçiniz</label>
+							<select id="states" class="form-control" name="b_bolge">
+    </tr><br>
+	<br>
+	
+	
+	
+	<td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Hizmetimizden Memnun Musunuz?</td>
+	
+	<input type="radio" name="soru" value="evet"/>Evet<br/>
+
+	<input type="radio" name="soru" value="hayır"/>Hayır<br/>
+	<br>
+
+<input type="submit" name="submit" value="Cevabımı ilet"></input>
+
+</form>
